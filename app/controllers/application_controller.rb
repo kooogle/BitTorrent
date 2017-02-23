eaa6fe4789ae33ff4fe8170a329abab7b82dab18
Koogle.Zhang <koogle.zhang@gmail.com>
2017-02-23 16:28:41 +0800

    生产环节添加缓存5分钟

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
+  before_action :allow_page_caching
+
   rescue_from CanCan::AccessDenied do |exception|
     if current_user
       respond_to do |format|
@@ -12,6 +14,12 @@ class ApplicationController < ActionController::Base
         format.html { redirect_to sign_in_path }
       end
     end
+  end
+
+  private
+
+  def allow_page_caching
+    expires_in(5.minutes) unless Rails.env.development?
   end
 
 end
