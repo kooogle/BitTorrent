15f8bddf53986389786106e0f03bcd9bfa044a18
Koogle.Zhang <koogle.zhang@gmail.com>
2017-02-23 17:25:52 +0800

    去除网页缓存

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
-  before_action :allow_page_caching
+  # before_action :allow_page_caching
 
   rescue_from CanCan::AccessDenied do |exception|
     if current_user
